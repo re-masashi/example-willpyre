96742ce17327a7cbccd668645a9c25872388aeae
Nafi Amaan Hossain <nafines007.com>
2023-03-05 02:39:18 +0530

    Temporary fix to mixed headers

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import willpyre
+import socketio
 from routes import router
 
 app = willpyre.App(router)
+
+
+sio = socketio.AsyncServer(async_mode='asgi')
+
+@sio.event
+async def connect(sid, environ, auth):
+	await sio.emit('ping')
+	print('connect ', sid)
+
+@sio.event
+async def pong(sid):
+    print('pong ', sid)
+
+app = socketio.ASGIApp(sio, other_asgi_app=app)
--- routes.py
+++ routes.py
@@ -11,6 +11,7 @@ from sqlalchemy import (
     insert,
 )
 from tables import users_table, books_table
+from dbutils import login_check, get_user
 from itsdangerous import URLSafeSerializer
 
 engine = create_engine("sqlite:///DB.sqlite", echo=True)
@@ -24,7 +25,8 @@ environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
 templates = {
     "index": environment.get_template("index.html"),
     "login": environment.get_template("login.html"),
-    "signup": environment.get_template("signup.html")
+    "signup": environment.get_template("signup.html"),
+    "pop": environment.get_template("pop")
 }
 
 
@@ -56,22 +58,21 @@ async def index(req, res):
     res.body = templates["index"].render(loggedin=loggedin, showcase=products)
     return res
 
-
 @router.get('/static/:*filepath')
 async def file_hosting(req, res):
     filepath = req.params.get('filepath')
     path = 'static/'
     for part in filepath:
         path += part+'/'
-    re.sub("\.\./", "", path)
+    re.sub("\.\./", "", path) # prevent users from climbing up the directory
     path = path[:-1]
     if os.path.exists(path):
         res.body = open(path).read()
-        res.headers["content-type"] = mimetypes.guess_type(path)[0]
+        res.headers["connection"] = "close" # No need of keep-alive
+        res.content_type = mimetypes.guess_type(path)[0]
     else:
         res = Response404()
     return res
-
 
 @router.get('/login')
 async def login_get(req, res):
@@ -80,29 +81,15 @@ async def login_get(req, res):
     res.body = templates["login"].render()
     return res
 
-
 @router.post('/login')
 async def login_post(req, res):
     if await is_logged_in(req):
         return Redirect('/', status=303)
 
-    select_stmt = select(users_table).where(
-        users_table.c.usertag == req.body.get('tag', " ")[1:])
-
-    with engine.connect() as conn:
-        user_cur = conn.execute(select_stmt)
-        users = []
-        for user in user_cur:
-            print(user)
-            users += [{
-                "name": user[0],
-                "usertag": user[1],
-                "password": user[2]
-            }]
-    if users == []:
+    user = await get_user(req.body.get('tag', "")[1:])
+    if user == None:
         return Redirect('/signup', status=303)
-    user = users[0]
-    if await login_db(req.body.get('tag')[1:], req.body.get('password'), user):
+    if await login_check(req.body.get('tag')[1:], req.body.get('password'), user):
         res.cookies["sessionid"] = Cookie(
             serializer.dumps(
             users[0]["usertag"]), 
@@ -113,42 +100,32 @@ async def login_post(req, res):
     res.body = "Wrong usertag or password"
     return res
 
-
-@router.post('/signup')
-async def signup_post(req, res):
-    if await is_logged_in(req):
-        return Redirect('/', status=303)
-    select_stmt = select(users_table).where(
-        users_table.c.usertag == req.body.get('tag'))
-    with engine.connect() as conn:
-        user_cur = conn.execute(select_stmt)
-        user_count = 0
-        for user in user_cur:
-            user_count += 1
-    if user_count != 0:
-        return Redirect('/', status=303)
-
-    print(hashlib.sha512(req.body.get('password').encode()+db_salt).hexdigest())
-    insert_stmt = insert(users_table).values(name=req.body.get('username'),
-                                             usertag=req.body.get('tag')[1:],
-                                             password=hashlib.sha512(req.body.get('password').encode('utf-8')+db_salt).hexdigest())
-    with engine.connect() as conn:
-        result = conn.execute(insert_stmt)
-        conn.commit()
-        print("user made")
-    return Redirect('/login', status=303)
-
-
 @router.get('/signup')
 async def signup_get(req, res):
     res.body = templates["signup"].render()
     return res
 
 
-async def login_db(usertag, password, user):
-    if usertag != user["usertag"]:
-        return False
-    digest = hashlib.sha512(password.encode('utf-8')+db_salt).hexdigest()
-    if digest != user["password"]:
-        return False
-    return True
+@router.post('/signup')
+async def signup_post(req, res):
+    if await is_logged_in(req):
+        return Redirect('/', status=303)
+    user = await get_user(req.body.get('tag', ""))
+    if user_count != None:
+        return Redirect('/login', status=303)
+
+    insert_stmt = insert(users_table).values(name=req.body.get('username'),
+                                             usertag=req.body.get('tag')[1:],
+                                             password=hashlib.sha512(req.body.get('password').encode('utf-8')+db_salt).hexdigest())
+    with engine.connect() as conn:
+        result = conn.execute(insert_stmt)
+        conn.commit()
+        print("user created")
+    return Redirect('/login', status=303)
+
+
+@router.get('/pop')
+async def pop_get(req, res):
+    res.body = templates["pop"].render()
+    return res
+
